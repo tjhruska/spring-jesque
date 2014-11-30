289d1af7cb1f2f34077030e2524635ace0422f83
Timothy Hruska <tjhruska@yahoo.com>
2014-11-30 11:34:51 -0600

    Added bean name logging to jesque container.

--- src/main/java/com/tjhruska/spring/jesque/JesqueContainer.java
+++ src/main/java/com/tjhruska/spring/jesque/JesqueContainer.java
@@ -22,38 +22,43 @@ import net.greghaines.jesque.worker.Worker;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.BeanNameAware;
 
 /**
- * JesqueContainer maintains a group of workers that are all created using the same injected workerFactory.
- * 
+ * JesqueContainer maintains a group of workers that are all created using the
+ * same injected workerFactory.
+ *
  * @author Timothy Hruska <https://github.com/tjhruska>
  *
  */
-public class JesqueContainer {
+public class JesqueContainer implements BeanNameAware {
   private static final Logger log = LoggerFactory.getLogger(JesqueContainer.class);
-  
+
   private Callable<Worker> workerFactory;
 
   private Integer maxWorkerCount;
   private Worker[] workers;
   private Thread[] workerThreads;
   private boolean paused;
+  private String beanName;
 
   public Worker[] getWorkers() {
     return workers;
   }
-  
+
   public Thread[] getWorkerThreads() {
     return workerThreads;
   }
-  
+
   public Integer getMaxWorkerCount() {
     return maxWorkerCount;
   }
 
   /**
    * This JesqueContainer that will hold exactly one worker.
-   * @param workerFactory that will create/replenish the workers for this container
+   *
+   * @param workerFactory
+   *          that will create/replenish the workers for this container
    */
   public JesqueContainer(Callable<Worker> workerFactory) {
     this.workerFactory = workerFactory;
@@ -62,11 +67,14 @@ public class JesqueContainer {
     this.workerThreads = new Thread[maxWorkerCount];
     this.paused = false;
   }
-  
+
   /**
    * This JesqueContainer that will hold up to maxWorkerCount.
-   * @param workerFactory that will create/replenish the workers for this container
-   * @param maxWorkerCount up to this number of workers will be maintained in this container
+   *
+   * @param workerFactory
+   *          that will create/replenish the workers for this container
+   * @param maxWorkerCount
+   *          up to this number of workers will be maintained in this container
    */
   public JesqueContainer(Callable<Worker> workerFactory, Integer maxWorkerCount) {
     this.workerFactory = workerFactory;
@@ -75,11 +83,11 @@ public class JesqueContainer {
     this.workerThreads = new Thread[maxWorkerCount];
     this.paused = false;
   }
-  
+
   /**
-   * Spin through to attempt to replace any dead/missing workers.  
-   * WorkerFactory is not required to return a worker.  
-   *    (This is mostly useful when the workers are tied to limited resources.)
+   * Spin through to attempt to replace any dead/missing workers. WorkerFactory
+   * is not required to return a worker. (This is mostly useful when the workers
+   * are tied to limited resources.)
    */
   public void checkWorkers() {
     for (int i = 0; i < maxWorkerCount; i++) {
@@ -91,21 +99,22 @@ public class JesqueContainer {
           throw new RuntimeException("Failed to get a worker from the workerFactory", e);
         }
         if (worker != null) {
-          log.info("Started worker(s) of type '{}' with queues: '{}'", worker.getName(), worker.getQueues());
-    
-			    Thread workerThread = new Thread(worker);
-			    workerThread.setDaemon(false);
-			    workerThread.start();
-        
-			    workers[i] = worker;
-			    workerThreads[i] = workerThread;
+          log.info(beanName + " started worker(s) of type '{}' with queues: '{}'", worker.getName(), worker.getQueues());
+
+          Thread workerThread = new Thread(worker);
+          workerThread.setDaemon(false);
+          workerThread.start();
+
+          workers[i] = worker;
+          workerThreads[i] = workerThread;
         }
       }
     }
   }
-  
+
   /**
    * This does not check the state of the contained workers.
+   *
    * @return current pause state of the container.
    */
   public boolean isPaused() {
@@ -113,31 +122,35 @@ public class JesqueContainer {
   }
 
   /**
-   * Container will remember the last pause state, and only send changes to workers.
-   * Workers will pause after they complete any currently running jobs.
-   * @param paused state to send to the workers if changing
+   * Container will remember the last pause state, and only send changes to
+   * workers. Workers will pause after they complete any currently running jobs.
+   *
+   * @param paused
+   *          state to send to the workers if changing
    */
   public void togglePause(boolean paused) {
     if (this.paused == paused) {
-       return;
+      return;
     }
     this.paused = paused;
     for (int i = 0; i < maxWorkerCount; i++) {
       if (workerThreads[i] != null && workerThreads[i].isAlive()) {
         if (paused) {
-		      log.info("pausing worker(s) '{}' with queues '{}'", workers[i].getName(), workers[i].getQueues());
-		    } else {
-		      log.info("unpausing worker(s) '{}' with queues '{}'", workers[i].getName(), workers[i].getQueues());
-		    }
-		    workers[i].togglePause(paused);
+          log.info("pausing worker(s) '{}' with queues '{}'", workers[i].getName(), workers[i].getQueues());
+        } else {
+          log.info("unpausing worker(s) '{}' with queues '{}'", workers[i].getName(), workers[i].getQueues());
+        }
+        workers[i].togglePause(paused);
       }
     }
   }
 
   /**
-   * Sends end() message to all currently live workers.  
-   * If now is false workers won't die until they complete current jobs.
-   * @param now if true will abort currently running jobs.
+   * Sends end() message to all currently live workers. If now is false workers
+   * won't die until they complete current jobs.
+   *
+   * @param now
+   *          if true will abort currently running jobs.
    */
   public void stop(boolean now) {
     for (int i = 0; i < maxWorkerCount; i++) {
@@ -147,10 +160,13 @@ public class JesqueContainer {
       }
     }
   }
-  
+
   /**
    * Wait on any currently live worker threads until they exit.
-   * @param millis how long to wait for each worker before moving on to join the next worker
+   *
+   * @param millis
+   *          how long to wait for each worker before moving on to join the next
+   *          worker
    * @throws InterruptedException
    */
   public void join(long millis) throws InterruptedException {
@@ -161,4 +177,9 @@ public class JesqueContainer {
       }
     }
   }
-} 
+
+  @Override
+  public void setBeanName(String name) {
+    this.beanName = name;
+  }
+}
--- src/main/java/com/tjhruska/spring/jesque/SpringConfiguredJesqueServer.java
+++ src/main/java/com/tjhruska/spring/jesque/SpringConfiguredJesqueServer.java
@@ -20,40 +20,50 @@ import java.util.Collection;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.BeanNameAware;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
 /**
  * Manages the spring life-cycle for a collection of jesque containers.
+ *
  * @author Timothy Hruska <https://github.com/tjhruska>
  *
  */
-public class SpringConfiguredJesqueServer extends Thread implements InitializingBean, DisposableBean {
+public class SpringConfiguredJesqueServer extends Thread implements InitializingBean, DisposableBean, BeanNameAware {
   private static final Logger log = LoggerFactory.getLogger(SpringConfiguredJesqueServer.class);
-  
+
   private Collection<JesqueContainer> jesqueContainers;
   private Integer sleepTime;
   private Boolean shutdown;
-  
+  private String beanName;
+
   /**
-   * Allows for configuration of the sleepTime between calls to each jesqueContainer.checkWorkers()
-   * The jesqueContainer only updates is collection of workers during this call.
-   * Especially usefull if you want a responsive system, and utilize WorkerExitOnEmpty workers.
-   * @param jesqueContainers containers of jesque workers
-   * @param sleepTime time to sleep between calls to jesqueContainer.checkWorkers()
+   * Allows for configuration of the sleepTime between calls to each
+   * jesqueContainer.checkWorkers() The jesqueContainer only updates is
+   * collection of workers during this call. Especially usefull if you want a
+   * responsive system, and utilize WorkerExitOnEmpty workers.
+   *
+   * @param jesqueContainers
+   *          containers of jesque workers
+   * @param sleepTime
+   *          time to sleep between calls to jesqueContainer.checkWorkers()
    */
-  public SpringConfiguredJesqueServer( Collection<JesqueContainer> jesqueContainers, Integer sleepTime) {
+  public SpringConfiguredJesqueServer(Collection<JesqueContainer> jesqueContainers, Integer sleepTime) {
     this.jesqueContainers = jesqueContainers;
     this.sleepTime = sleepTime;
   }
 
   /**
-   * In the general case workers will not exit.  So there is little need to poll the jesqueContainer.
-   * Worker trap exceptions, but in the off chance of an issue we still poll the jesqueContainer.checkWorkers().
-   * Default sleepTime is 5 minutes.
-   * @param jesqueContainers containers of jesque workers
+   * In the general case workers will not exit. So there is little need to poll
+   * the jesqueContainer. Worker trap exceptions, but in the off chance of an
+   * issue we still poll the jesqueContainer.checkWorkers(). Default sleepTime
+   * is 5 minutes.
+   *
+   * @param jesqueContainers
+   *          containers of jesque workers
    */
-  public SpringConfiguredJesqueServer( Collection<JesqueContainer> jesqueContainers) {
+  public SpringConfiguredJesqueServer(Collection<JesqueContainer> jesqueContainers) {
     this.jesqueContainers = jesqueContainers;
     this.sleepTime = 30000;
   }
@@ -61,15 +71,18 @@ public class SpringConfiguredJesqueServer extends Thread implements Initializing
   public Collection<JesqueContainer> getJesqueContainers() {
     return jesqueContainers;
   }
-  
+
   public Boolean isShutdown() {
     return shutdown;
   }
 
-  /** 
-   * Called by spring after all beans have been created.  Starts jesqueContainers.
+  /**
+   * Called by spring after all beans have been created. Starts
+   * jesqueContainers.
+   *
    * @see org.springframework.beans.factory.InitializingBean#afterPropertiesSet()
    */
+  @Override
   public void afterPropertiesSet() throws Exception {
     log.info("Starting jesque server.");
     shutdown = false;
@@ -78,56 +91,68 @@ public class SpringConfiguredJesqueServer extends Thread implements Initializing
   }
 
   /**
-   * Called by spring on shutdown.  Does not wait for jobs to stop.  
-   * The non-daemon server thread should wait for workers to finish.
+   * Called by spring on shutdown. Does not wait for jobs to stop. The
+   * non-daemon server thread should wait for workers to finish.
+   *
    * @see java.lang.Thread#destroy()
    */
+  @Override
   public void destroy() {
     log.info("Stopping jesque server.");
-    
-    for(JesqueContainer jesqueContainer : jesqueContainers) {
+
+    for (JesqueContainer jesqueContainer : jesqueContainers) {
       jesqueContainer.stop(false);
     }
-    
+
     shutdown = true;
   }
-  
+
   /**
-   * Send the pause state requested down to each jesqueContainer.
-   * Any currently running jobs will finish, and the worker will then stop taking new jobs.
-   * @param paused new state requested for jesqueContainers
+   * Send the pause state requested down to each jesqueContainer. Any currently
+   * running jobs will finish, and the worker will then stop taking new jobs.
+   *
+   * @param paused
+   *          new state requested for jesqueContainers
    * @see com.tjhruska.spring.jesque.JesqueContainer#togglePause(boolean)
    */
   public void togglePause(boolean paused) {
-    for(JesqueContainer jesqueContainer : jesqueContainers) {
+    for (JesqueContainer jesqueContainer : jesqueContainers) {
       jesqueContainer.togglePause(paused);
     }
   }
-  
+
   /**
-   * This shouldn't be called in production, it will be handled by the spring lifecycle.
+   * This shouldn't be called in production, it will be handled by the spring
+   * lifecycle.
+   *
    * @see java.lang.Thread#run()
    */
+  @Override
   public void run() {
-    log.info("Jesque server starting run loop.");
+    log.info(beanName + " jesque server starting run loop.");
     while (!shutdown) {
-      for(JesqueContainer jesqueContainer : jesqueContainers) {
-	      jesqueContainer.checkWorkers();
-	    }
+      for (JesqueContainer jesqueContainer : jesqueContainers) {
+        jesqueContainer.checkWorkers();
+      }
       try {
         sleep(sleepTime);
       } catch (InterruptedException e) {
         throw new RuntimeException(e);
       }
     }
-    log.info("Jesque server exiting run loop.  Joining to any still running workers.");
-    
-    for(JesqueContainer jesqueContainer : jesqueContainers) {
+    log.info(beanName + " jesque server exiting run loop.  Joining to any still running workers.");
+
+    for (JesqueContainer jesqueContainer : jesqueContainers) {
       try {
         jesqueContainer.join(0);
       } catch (InterruptedException e) {
-        log.info("Interrupted during join to jesqueContainer, moving on.");
+        log.info(beanName + " interrupted during join to jesqueContainer, moving on.");
       }
     }
+  }
+
+  @Override
+  public void setBeanName(String name) {
+    this.beanName = name;
   }
 }
